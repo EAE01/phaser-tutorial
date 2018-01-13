949e925c6a9b23dea63c99dd4e21272a89925ae9
Euan Edgar <EAE01@live.co.uk>
2018-01-13 20:23:26 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+var jsDirectory = ['js/**/*.js', '!js/vendor/**/*', '!js/compiled/**/*'];
+var scssDirectory = ['css/**/*.scss', '!css/compiled/**/*'];
+
+gulp.task('styles', stylesTask);
+gulp.task('scripts', scriptsTask);
+gulp.task('watch', watchTask);
+gulp.task('default', ['styles'], defaultTask);
+
+function defaultTask(done) {
+  done();
+}
+
+function watchTask(done) {
+  gulp.watch(scssDirectory, ['styles'])
+  gulp.watch(jsDirectory, ['scripts'])
+  done();
+}
+
+function stylesTask(done) {
+  gulp.src(scssDirectory)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('application.css'))
+    .pipe(gulp.dest('css/compiled/'));
+  done()
+}
+
+function scriptsTask(done) {
+  gulp.src(jsDirectory)
+    .pipe(concat('application.js'))
+    .pipe(gulp.dest('js/compiled/'));
+  done();
+}
